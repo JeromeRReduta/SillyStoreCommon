9d19f1435347a2f99c68ba7ba3b60d50c588e17a
JRReduta <jrreduta@yahoo.com>
2026-04-25 01:58:42 -0700

    (change) update request impl

--- dtos/requests/create-requests/ICreateOrderRequest.ts
+++ dtos/requests/create-requests/ICreateOrderRequest.ts
@@ -1,4 +1,7 @@
+import { OrderStatus } from "../../../domain-objects/Order.ts";
+
 export interface ICreateOrderRequest {
     readonly dateStr: string;
     readonly userId: number; // even if you're an admin you need to add your id here
+    readonly status: OrderStatus;
 }
