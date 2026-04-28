069a3730e005193d2582eec3c9ee27065c00631e
JRReduta <jrreduta@yahoo.com>
2026-04-28 01:03:24 -0700

    (refactor) changed creatorId from number to required<ICartItem[creatorId]>

--- dtos/cartItemDtos.ts
+++ dtos/cartItemDtos.ts
@@ -7,21 +7,15 @@ export type ICreateCartItemRequest = ICartItem;
 export type IGetAllCartItemsRequest = ICartItem & Pick<IUser, "role">;
 export type IGetCartItemRequest = Pick<ICartItem, "orderId" | "productId" | "creatorId"> &
     Pick<IUser, "role">;
-
 export type IUpdateCartItemRequest = ICartItem & Pick<IUser, "role">;
 export type IDeleteCartItemRequest = Pick<ICartItem, "orderId" | "productId" | "creatorId"> &
     Pick<IUser, "role">;
-
 export type IGetOrdersIncludingProductRequest = Pick<ICartItem, "productId" | "creatorId"> &
     Pick<IUser, "role">;
-
 export type IGetPendingCartItemsRequest = Pick<ICartItem, "creatorId"> & Pick<IUser, "role">;
-
-export type IGetCartItemsRequest = IGetPendingCartItemsRequest &
-    Pick<ICartItem, "productId" | "orderId">;
-
+export type IGetCartItemsRequest = IGetPendingCartItemsRequest & Pick<ICartItem, "orderId">;
 export interface IMergePendingCartItemsRequest extends Pick<IUser, "role"> {
-    readonly creatorId: number;
+    readonly creatorId: Required<ICartItem["creatorId"]>;
     readonly items: Array<Pick<ICartItem, "productId" | "quantity">>;
 }
 export type IMergeCartItemsRequest = IMergePendingCartItemsRequest & Pick<ICartItem, "orderId">;
